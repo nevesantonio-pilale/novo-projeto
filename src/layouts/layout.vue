acb6ac6eb7f14a8fb6b3189d09ee19d339ac2410
Neves António <90612989+nevesantonio-pilale@users.noreply.github.com>
2022-03-04 15:44:28 +0200

    Initial commit

---
+++ src/layouts/layout.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <q-layout view="lHh lpr lFf" container style="height: 100vh" class="shadow-2 rounded-borders">
+      <q-header reveal elevated>
+        <q-toolbar>
+          <q-avatar>
+            <img src="../assets/perfil.jpg" alt="">
+          </q-avatar>
+
+          <q-toolbar-title> App de Apresentação </q-toolbar-title>
+
+          <q-btn flat round dense icon="whatshot" />
+        </q-toolbar>
+      </q-header>
+
+      <q-footer reveal elevated>
+        <q-toolbar>
+        </q-toolbar>
+      </q-footer>
+
+      <q-page-container>
+        <q-page class="q-pa-md">
+          <q-page class="flex flex-center">
+            <div class="q-pa-md q-gutter-sm">
+              <q-avatar style="width:200px; height: 200px">
+                <img src="../assets/perfil.jpg" alt="">
+              </q-avatar>
+            </div>
+              <p class="q-D-t" style="font-size: 20pt" > Neves António </p>
+            </q-page>
+        </q-page>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+/* import EssentialLink from 'components/EssentialLink.vue'
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}) */
+</script>
---
+++ src/pages/apresentacao.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+  <div class="q-pa-md q-gutter-sm">
+    <q-avatar style="width:200px; height: 200px">
+      <img src="https://cdn.quasar.dev/img/avatar.png">
+    </q-avatar>
+  </div>
+
+    <p style="font-size: 20pt"> Neves Antonio </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
